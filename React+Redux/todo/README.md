5732ab813a9558a0940f02048353302cc0521e75
Панов Владимир Сергеевич <v.panov@2gis.ru>
2019-09-10 14:53:54 +0700

    Start

--- React+Redux/todo/README.md
+++ React+Redux/todo/README.md
@@ -1,2 +1,1 @@
-Todo Application
------
+Todo Appliation
--- React+Redux/todo/src/index.js
+++ React+Redux/todo/src/index.js
@@ -1,7 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/app';
-
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+console.log('Hello World');
